f4cc1de9abc7c25710990ee01040e2a21d12c7c0
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-18 04:40:48 -0400

    ch6 objects initial commit

---
+++ Ch6Objects.js
@@ -0,0 +1,13 @@
+//Chapter 6
+//Objects
+
+//Encapsulation
+
+//Methods
+//Properties that hold function values
+let rabbit = {};
+rabbit.speak = function(line){
+    console.log(`The rabbit says '${line}'`);
+};
+
+rabbit.speak("I'm alive.");
