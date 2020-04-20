31837354042749f29c7214a47b96d2b388dd1334
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-20 15:17:15 -0400

    ch8

--- ch8debugging.js
+++ ch8debugging.js
@@ -1,3 +1,20 @@
 //Chapter 8
 //Debugging
 
+//Language
+
+//Strict mode
+
+//Types
+
+//Testing
+
+//Debugging 
+
+//Error Propagation
+
+//Exceptions
+
+//Cleaning up after exceptions
+
+//Selectively catching
