3588d321030b4228ffa239b662387a18045d9903
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-26 08:41:22 -0400

    ch12

--- Ch12ProgrammingLanguage.js
+++ Ch12ProgrammingLanguage.js
@@ -1,3 +1,10 @@
 //Chapter 12
 
 //Making a Programming Language
+
+//Parsing
+//Reads text and produces data structures.
+//If text doesn't form valid,point error.
+
+//Everything in egg is an expression. 
+//Bindin,g number, string, or application.
